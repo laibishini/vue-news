99c4ee8e3ce1e9244b22147e336d897d93ed51e5
huhao <674698095@qq.com>
2017-08-03 22:48:31 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    
+<NavHeader></NavHeader>
+
+
+
+
+  </div>
+</template>
+
+<script>
+
+import NavHeader from './components/Nav.vue'
+
+export default {
+  components:{
+    NavHeader
+  }
+
+
+
+}
+</script>
+
+<style>
+
+</style>
